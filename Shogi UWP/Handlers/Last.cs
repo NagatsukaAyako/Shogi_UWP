f64a1544cc37d3503254ab3b47f42374003f322e
Dmitriy Stakhov <StakhovDmitriy@outlook.com>
2018-04-21 16:16:18 +0300

    Добавлен Last class

---
+++ Shogi UWP/Handlers/Last.cs
@@ -0,0 +1,17 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace Shogi_UWP.Handlers
+{
+    class Last : Grid
+    {
+        public Last()
+        {
+            BorderBrush = new SolidColorBrush(Windows.UI.Colors.Black);
+            BorderThickness = new Thickness(2);
+            Background = new SolidColorBrush(Windows.UI.Color.FromArgb(105, 155, 199, 0));
+            IsHitTestVisible = false;
+        }
+    }
+}
